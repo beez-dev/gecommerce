e1a808737c6efe5f588b0625d326160b7fd48cac
rareorigin <product@commerceroad.finance>
2025-07-28 23:23:12 +0545

    *

--- node-services/migrations/config.ts
+++ node-services/migrations/config.ts
@@ -1,8 +1,19 @@
 import { dbConfig as conf } from '@gecommerce/config';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const db = conf.database;
 
 console.log({ db });
+
+// const db = {
+//     user: 'nodem',
+//     password: 'nodemuser',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'gecommerce',
+// };
 
 export const config = {
     connectionUrl: `postgresql://${db.user}:${db.password}@${db.host}:${db.port}/${db.database}`,
